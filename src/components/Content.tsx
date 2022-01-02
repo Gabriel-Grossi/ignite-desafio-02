f2ebb632f552f5061b8d3311d01e06e1551c2256
Gabriel Grossi <gabrigrossi@hotmail.com.br>
2022-01-02 19:28:32 -0300

    Desafio Finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
+import { MovieCard } from "./MovieCard";
+import { useMovie } from "../hooks/useMovie";
+
+
 export function Content() {
   // Complete aqui
+  const {movies, selectedGenre} = useMovie();
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
